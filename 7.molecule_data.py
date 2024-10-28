e75bd3a5c833a6738bda06c51d7d38eccf78e55f
chen <151988026+314159287@users.noreply.github.com>
2024-10-28 15:24:33 +0800

    7.molecule_data.py

--- 7.molecule_data.py
+++ 7.molecule_data.py
@@ -15,7 +15,7 @@ import csv
 # 在chrome加载
 chrome_options = Options()
 # chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')  # 禁用GPU
+# chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('start-maximized')
